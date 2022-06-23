90072c01a3670e43a8398001e551fe037329a3d6
Koichi Hirao - MSFT <koichih@microsoft.com>
2022-06-23 12:07:32 -0700

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -14,4 +14,4 @@ echo "$(date +'%Y-%m-%d %H:%M:%S')    post-create complete" >> "$HOME/status"
 
 # install E4K componentns as part of Codespace creation
 helm install e4k oci://edgecharts.azurecr.io/helm/az-e4k --set-file e4kspiffe.identitymanager.configToml=’/workspaces/kubernetes-in-codespaces/.devcontainer/override-identities.toml’ --set-string e4kmqtt.authenticator.type=”null”
-kubectl apply –f /workspaces/kubernetes-in-codespaces/.devcontainer/mqttclient.yaml
+kubectl apply -f /workspaces/kubernetes-in-codespaces/.devcontainer/mqttclient.yaml

e82259662aba8a944e593effb4b0432efdc9f06a
Koichi Hirao - MSFT <koichih@microsoft.com>
2022-08-16 16:00:58 -0700

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -13,5 +13,10 @@ echo "post-create complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-create complete" >> "$HOME/status"
 
 # install E4K componentns as part of Codespace creation
-helm install e4k oci://edgecharts.azurecr.io/helm/az-e4k --set-file e4kspiffe.identitymanager.configToml='/workspaces/kubernetes-in-codespaces/.devcontainer/override-identities.toml' --set e4kspiffe.server.jwt.key_ttl=1000
+helm install e4k oci://edgecharts.azurecr.io/helm/az-e4k -n e4k --create-namespace
 kubectl apply -f /workspaces/kubernetes-in-codespaces/.devcontainer/mqttclient.yaml
+
+# install E4K CLI
+OS='linux'; ARCH='amd64'; curl -sSL -o kubectl-aziot_e4k-linux-amd64.tar.gz https://github.com/Azure/e4k-cli/releases/tag/v0.0.1/kubectl-aziot_e4k-$OS-$ARCH.tar.gz;
+tar -xzf kubectl-aziot_e4k-linux-amd64.tar.gz
+cp kubectl-aziot_e4k /usr/local/bin
